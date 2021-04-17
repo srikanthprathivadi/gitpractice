e4f7557626e0b51ec9ca62387a31662c57d0335a
Srini <troubleshootingandtweaks@gmail.com>
2021-04-17 15:21:26 +0000

    changes to sysout

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello.java
 {
 public static void main(String args[])
 {
-System.out.println("hello Master");
+System.out.println("hello remove Master");
+System.out.println("hello child"); 
 }
 }
